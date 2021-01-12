d8872a6b7805c62ce86952a2e8bef970345067ba
User <alekseykarpelev@gmail.com>
2021-01-12 12:25:13 +1000

    add ui package
    use  -d memory_limit=-1  for install

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,12 +19,13 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-            $table->string('api_token', 80)->after('password')
+            $table->string('api_token', 80)
                 ->unique()
                 ->nullable()
                 ->default(null);
+            $table->rememberToken();
+            $table->timestamps();
+
         });
     }
 
