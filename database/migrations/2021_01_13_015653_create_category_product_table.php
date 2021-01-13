ed1bf42c57f954855888e4e808086708418ff20b
User <alekseykarpelev@gmail.com>
2021-01-13 12:04:14 +1000

    правки

--- database/migrations/2021_01_13_015653_create_category_product_table.php
+++ database/migrations/2021_01_13_015653_create_category_product_table.php
@@ -13,14 +13,13 @@ class CreateCategoryProductTable extends Migration
      */
     public function up()
     {
-      
+
             Schema::create('category_product', function (Blueprint $table) {
-                $table->id();
+                $table->bigIncrements('id');
                 $table->unsignedBigInteger('category_id');
                 $table->unsignedBigInteger('product_id');
                 $table->foreign('category_id')->references('id')->on('categories');
                 $table->foreign('product_id')->references('id')->on('products');
-            });
         });
     }
 
